f167f108122d855a4b2df298e0f63bf9acf2856d
mdcarmo <marc29dias@yahoo.com.br>
2023-02-26 17:58:53 -0300

    Alteraçoes para implementar o circuit-breaker

--- filme/src/main/java/br/com/marcdias/filme/avaliacaoClient/AvaliacaoClientImpl.java
+++ filme/src/main/java/br/com/marcdias/filme/avaliacaoClient/AvaliacaoClientImpl.java
@@ -1,38 +1,36 @@
 package br.com.marcdias.filme.avaliacaoClient;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class AvaliacaoClientImpl implements  AvaliacaoClient{
 
     private final Logger logger = LoggerFactory.getLogger(AvaliacaoClientImpl.class);
     private final RestTemplate restTemplate;
-
     private final static String API_URL = UriComponentsBuilder
             .fromHttpUrl("http://localhost:8081/avaliacoes")
             .queryParam("filmeId", "{filmeId}")
             .encode()
             .toUriString();
 
+    private final Map<Long, List<AvaliacaoModel>> CACHE_AVALIACAO = new HashMap<>();
     public AvaliacaoClientImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
+    @CircuitBreaker(name = "avaliacao-circuitBreaker", fallbackMethod = "getByFilmeIdInCache")
     public List<AvaliacaoModel> getByFilmeId(Long filmeId) {
         final List<AvaliacaoModel> avaliacoes = executarRequisicao(filmeId);
         return avaliacoes;
     }
-
     private List<AvaliacaoModel> executarRequisicao(Long filmeId) {
         final Map<String, Object> parametros = new HashMap<>();
         parametros.put("filmeId", filmeId);
@@ -46,6 +44,13 @@ public class AvaliacaoClientImpl implements  AvaliacaoClient{
             logger.error("Erro ao buscar avaliações");
             throw e;
         }
+        logger.info("Colocando em cache");
+        CACHE_AVALIACAO.put(filmeId, Arrays.asList(avaliacoes));
+
         return Arrays.asList(avaliacoes);
+    }
+    private List<AvaliacaoModel> getByFilmeIdInCache(Long filmeId, Throwable e){
+        logger.info("Buscando no cache a informação das avaliações de filme");
+        return CACHE_AVALIACAO.getOrDefault(filmeId, new ArrayList<>());
     }
 }
